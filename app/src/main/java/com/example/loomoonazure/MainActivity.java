710d4615834c0589d32469b3f14704de0db8cba1
Sean Kelly <home@seank.com>
2019-08-07 18:05:52 -0700

    UI and robot functionality updates

--- app/src/main/java/com/example/loomoonazure/MainActivity.java
+++ app/src/main/java/com/example/loomoonazure/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.loomoonazure;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.segway.robot.sdk.base.bind.ServiceBinder;
 import com.segway.robot.sdk.locomotion.head.Head;
+import com.segway.robot.sdk.locomotion.sbv.Base;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -16,78 +20,169 @@ public class MainActivity extends AppCompatActivity {
     private float headYaw = 0;
     private float headPitch = 0;
     private float headYawDelta = 0.1f;
-    private float headPitchDelta = 0.03f;
-    private float maxRadians = 1.5708f;
+    private float headPitchDelta = 0.1f;
+    private float maxYawRadians = 1.5708f;
+    private float maxPitchRadians = 0.7854f;
 
-    Head robotHead;
-    boolean isHeadBind = false;
+    private static final int FORWARD = 0;
+    private static final int BACK = 1;
+    private static final int LEFT = 2;
+    private static final int RIGHT = 3;
+
+    private int baseDirection = FORWARD;
+
+    private Head robotHead;
+    private boolean isHeadBind = false;
+    private Base robotBase;
+    private boolean isBaseBind = false;
 
     TextView output;
     ScrollView container;
+    Button process;
+    Button direction;
 
-    Timer headTimer = new Timer();
-    TimerTask headTimerTask = new TimerTask() {
-        @Override
-        public void run() {
-            runOnUiThread(new Runnable() {
+    Timer timer = null;
+    TimerTask timerTask = null;
+
+    private void scheduleTimer() {
+        if (isHeadBind && isBaseBind) {
+            timer = new Timer();
+            timerTask = new TimerTask() {
                 @Override
                 public void run() {
-                    if (isHeadBind) {
-                        float yaw = robotHead.getWorldYaw().getAngle();
-                        float pitch = robotHead.getWorldPitch().getAngle();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            float yaw = 0;
+                            float pitch = 0;
+                            float angularVelocity = 0;
+                            float linearVelocity = 0;
 
-                        print("yaw: " + String.valueOf(yaw) + ", pitch: " + String.valueOf(pitch));
+                            if (isHeadBind) {
+                                yaw = robotHead.getWorldYaw().getAngle();
+                                pitch = robotHead.getWorldPitch().getAngle();
 
-                        headYaw += headYawDelta;
-                        headPitch += headPitchDelta;
-                        if (headYaw > maxRadians) {
-                            headYawDelta *= -1;
-                            headYaw = maxRadians;
-                        }
-                        if (headYaw < -maxRadians) {
-                            headYawDelta *= -1;
-                            headYaw = -maxRadians;
-                        }
+                                print("DETECT yaw: " + String.valueOf(yaw) + ", pitch: " + String.valueOf(pitch));
 
-                        if (headPitch > maxRadians) {
-                            headPitchDelta *= -1;
-                            headPitch = maxRadians;
-                        }
-                        if (headPitch < -maxRadians) {
-                            headPitchDelta *= -1;
-                            headPitch = -maxRadians;
-                        }
+                                headYaw += headYawDelta;
+                                if (Math.abs(headYaw) > maxYawRadians) {
+                                    headYawDelta *= -1;
+                                    headYaw = (headYaw / Math.abs(headYaw)) * maxYawRadians;
+                                }
 
-                        robotHead.setWorldYaw(headYaw);
-                        robotHead.setWorldPitch(headPitch);
-                    }
+                                headPitch += headPitchDelta;
+                                if (Math.abs(headPitch) > maxPitchRadians) {
+                                    headPitchDelta *= -1;
+                                    headPitch = (headPitch / Math.abs(headPitch)) * maxPitchRadians;
+                                }
+
+                                print("SET yaw: " + String.valueOf(headYaw) + ", pitch: " + String.valueOf(headPitch));
+
+                                robotHead.setWorldYaw(headYaw);
+                                robotHead.setWorldPitch(headPitch);
+                            }
+
+                            if (isBaseBind) {
+                                angularVelocity = robotBase.getAngularVelocity().getSpeed();
+                                linearVelocity = robotBase.getLinearVelocity().getSpeed();
+
+                                print("DETECT angularVelocity: " + String.valueOf(angularVelocity) + ", linearVelocity: " + String.valueOf(linearVelocity));
+                            }
+                        }
+                    });
                 }
-            });
+            };
+
+            timer.schedule(timerTask, 250, 250);
+            process.setText(R.string.process_stop);
+            process.setEnabled(true);
         }
-    };
+    }
+
+    private void unscheduleTimer() {
+        if (!isHeadBind && !isBaseBind) {
+            process.setText(R.string.process_start);
+            process.setEnabled(true);
+        }
+    }
 
     private ServiceBinder.BindStateListener headServiceBindListener = new ServiceBinder.BindStateListener() {
         @Override
         public void onBind() {
+            print("Bind Head");
             isHeadBind = true;
             robotHead.setMode(Head.MODE_SMOOTH_TACKING);
             robotHead.resetOrientation();
-
-            headTimer.schedule(headTimerTask, 250, 250);
+            scheduleTimer();
         }
         @Override
         public void onUnbind(String reason) {
+            print("Unbind Head");
             isHeadBind = false;
+            unscheduleTimer();
         }
     };
 
-    private void print(String msg) {
+    private ServiceBinder.BindStateListener baseServiceBindListener = new ServiceBinder.BindStateListener() {
+        @Override
+        public void onBind() {
+            print("Bind Base");
+            isBaseBind = true;
+            scheduleTimer();
+        }
+        @Override
+        public void onUnbind(String reason) {
+            print("Unbind Base");
+            isBaseBind = false;
+            unscheduleTimer();
+        }
+    };
+
+    public void print(String msg) {
         String out = output.getText().toString();
         out += "\n" + msg;
         output.setText(out);
 
         int height = output.getHeight();
         container.scrollTo(0, height);
+    }
+
+    private void startProcess() {
+        robotHead.bindService(getApplicationContext(), headServiceBindListener);
+        robotBase.bindService(getApplicationContext(), baseServiceBindListener);
+    }
+
+    private void stopProcess() {
+        robotHead.unbindService();
+        robotBase.unbindService();
+        if (timer != null) {
+            timer.cancel();
+            timer = null;
+        }
+        if (timerTask != null) {
+            timerTask.cancel();
+            timerTask = null;
+        }
+    }
+
+    public void move(final float linear, final float angular) {
+        print("move(" + String.valueOf(linear) + ", " + String.valueOf(angular) + ")");
+        new Thread()  {
+            @Override
+            public void run() {
+                robotBase.setLinearVelocity(linear);
+                robotBase.setAngularVelocity(angular);
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    print(e.getMessage());
+                }
+
+                robotBase.setLinearVelocity(0);
+                robotBase.setAngularVelocity(0);
+            }
+        }.start();
     }
 
     @Override
@@ -97,25 +192,76 @@ public class MainActivity extends AppCompatActivity {
 
         output = (TextView)findViewById(R.id.output);
         container = (ScrollView)findViewById(R.id.container);
+        process = (Button)findViewById(R.id.process);
+        direction = (Button)findViewById(R.id.direction);
 
-        print("This is a test");
-        print("This is another test");
+        output.setText("");
+        print("onCreate");
 
         robotHead = Head.getInstance();
-
-        robotHead.bindService(getApplicationContext(), headServiceBindListener);
+        robotBase = Base.getInstance();
     }
 
     @Override
     protected void onDestroy() {
-        robotHead.unbindService();
-        if (headTimer != null) {
-            headTimer.cancel();
-            headTimer = null;
-        }
-        if (headTimerTask != null) {
-            headTimerTask.cancel();
-            headTimerTask = null;
+        super.onDestroy();
+
+        print("onDestroy");
+
+        stopProcess();
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.process:
+                process.setEnabled(false);
+                if (!isHeadBind && !isBaseBind) {
+                    startProcess();
+                } else {
+                    stopProcess();
+                }
+                break;
+            case R.id.clear:
+                output.setText("");
+                break;
+            case R.id.direction:
+                switch (baseDirection) {
+                    case FORWARD:
+                        baseDirection = BACK;
+                        direction.setText(R.string.direction_back);
+                        break;
+                    case BACK:
+                        baseDirection = LEFT;
+                        direction.setText(R.string.direction_left);
+                        break;
+                    case LEFT:
+                        baseDirection = RIGHT;
+                        direction.setText(R.string.direction_right);
+                        break;
+                    case RIGHT:
+                        baseDirection = FORWARD;
+                        direction.setText(R.string.direction_forward);
+                        break;
+                }
+                break;
+            case R.id.move:
+                if (isBaseBind) {
+                    switch (baseDirection) {
+                        case FORWARD:
+                            move(1f, 0);
+                            break;
+                        case BACK:
+                            move(-1f, 0);
+                            break;
+                        case LEFT:
+                            move(0, 1f);
+                            break;
+                        case RIGHT:
+                            move(0, -1f);
+                            break;
+                    }
+                }
+                break;
         }
     }
 }
